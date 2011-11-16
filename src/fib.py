605bc4132f185c0b2be495d0c4dc28bb119a50d8
HairyFotr <maticpotocnik@gmail.com>
2011-11-16 20:19:58 +0100

    first python commit

---
+++ src/fib.py
@@ -0,0 +1,18 @@
+#/bin/sh
+#@begin@
+#red 2
+memo = {0:0, 1:1}
+def fib(n):
+    if not n in memo:
+        memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
+
+#red k
+memok = {}
+def fib(n,k):
+    if not k in memok:
+        memok[k] = dict(zip(range(k), [0 for i in range(k-1)]+[1]))
+    if not n in memok[k]:
+        memok[k][n] = sum([fib(n-i-1, k) for i in range(k)])
+    return memok[k][n]
+#@end@
